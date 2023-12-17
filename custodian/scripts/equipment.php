eb204444da48133c7745e323aa47e9224b0ce4b6
Marc Recto <2200931@slu.edu.ph>
2023-12-18 03:18:19 +0800

    Redo custodian module

--- custodian/scripts/equipment.php
+++ custodian/scripts/equipment.php
@@ -1,94 +1,155 @@
-<!-- equipment.php -->
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <title>Equipment Dashboard</title>
-  <link rel="stylesheet" href="../styles/equipment.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-  <style>
-    /* Add a CSS rule to set the fixed size for the images */
-    .equipment img {
-      width: 100px; /* Set the desired width */
-      height: 100px; /* Set the desired height */
-      object-fit: cover; /* Preserve aspect ratio and cover the entire container */
-      border: 1px solid #ddd; /* Add a border for better visibility */
-      border-radius: 5px; /* Optional: Add border-radius for rounded corners */
-    }
-  </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--Box Icons [https://boxicons.com/usage]-->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <title>Rooms</title>
+  <link rel="icon" href='../public/assets/images/r-icon.svg' type="image/svg">
+  <link rel="stylesheet" href="../public/styles/custodian_dashboard.css">
 </head>
 
 <body>
-  <div class="container">
-    <!-- Navigation Bar -->
-    <nav>
-      <ul>
-        <li><a href="#" class="logo">
-            <img src="/logo.jpg" alt="">
-            <span class="nav-item">DashBoard</span>
-          </a></li>
-        <li><a href="index.php">
-            <i class="fas fa-home"></i>
-            <span class="nav-item">Home</span>
-          </a></li>
-        <li><a href="room.php">
-            <i class="fas fa-user"></i>
-            <span class="nav-item">Add Rooms</span>
-          </a></li>
-        <li><a href="equipment.php">
-            <i class="fas fa-wallet"></i>
-            <span class="nav-item">Add Equipment</span>
-          </a></li>
-        <li><a href="manage_rental.php">
-            <i class="fas fa-chart-bar"></i>
-            <span class="nav-item">Manage Rental</span>
-          </a></li>
-          <li><a href="logout.php">
-            <i class="fas fa-chart-bar"></i>
-            <span class="nav-item">Logout</span>
-          </a></li>
+  <!--START OF SIDEBAR-->
+  <section id="sidebar">
+    <a href="#" alt="Rentify Logo" class="logo-img">
+      <img src='../public/assets/images/r-icon.svg' alt="Rentify Logo" class="logo-img">
+    </a>
+    <ul class="side-menu top">
+      <li>
+        <a href="../pages/custodian_dashboard.html">
+          <i class='bx bxs-dashboard'></i>
+          <span class="text">Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="../scripts/room.php">
+          <i class='bx bxs-door-open'></i>
+          <span class="text">Rooms</span>
+        </a>
+      </li>
+      <li class="active">
+        <a href="../scripts/equipment.php">
+          <i class='bx bxs-cabinet'></i>
+          <span class="text">Equipments</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-clipboard'></i>
+          <span class="text">Transaction History</span>
+        </a>
+      </li>
+      <ul class="side-menu bottom">
+        <li>
+          <a href="#" class="report">
+            <i class='bx bxs-shield-minus'></i>
+            <span class="text">Report</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class='bx bxs-cog'></i>
+            <span class="text">Settings</span>
+          </a>
+        </li>
+        <li>
+          <a href="../pages/custodian_login.html" class="logout">
+            <i class='bx bx-log-out'></i>
+            <span class="text">Logout</span>
+          </a>
+        </li>
       </ul>
+  </section>
+  <!--END OF SIDEBAR-->
+  <!--START OF CONTENT-->
+  <section id="content">
+    <nav>
+      <i class='bx bx-menu-alt-left'></i>
+      <a href="#" class="nav-link">Categories</a>
+      <form action="#">
+        <div class="form-input">
+          <form action="equipment.php" method="GET" class="search-form">
+            <div class="form-input">
+              <input type="search" name="search" placeholder="Search for Room or Equipment"
+                value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search']) : ''; ?>">
+              <button type="submit" class="search-btn"><i class='bx bx-search'></i></button>
+            </div>
+          </form>
+        </div>
+      </form>
+      <!--Notification Feature-->
+      <a href="#" class="notification">
+        <i class='bx bxs-bell'></i>
+        <!--UPDATEABLE BY NUMBER OF REQUESTS/ORDERS-->
+        <span class="num">8</span>
+      </a>
+      <!--Profile Feature-->
+      <a href="#" class="profile">
+        <i class='bx bxs-user-circle'></i>
+      </a>
     </nav>
+  </section>
+  <!--END OF CONTENT-->
+  <!--ROOMS LIST-->
+  <?php
+  include 'equipment_db.php'; // Include your equipment database connection
+  
+  // Check for search query
+  $search = isset($_GET['search']) ? '%' . $mysqli->real_escape_string($_GET['search']) . '%' : '';
 
-    <!-- Equipment List -->
-    <div class="equipment-list">
-    <?php
-    include 'equipment_db.php';
+  // Retrieve and display equipment information with search filter if provided, otherwise show all equipments
+  if (!empty($search)) {
+    $sql = "SELECT * FROM equipment WHERE equipment_name LIKE ? OR equip_type LIKE ?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param('ss', $search, $search);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-    // Retrieve and display equipment information
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+        echo '<div class="equipment">';
+        echo '<h2>' . $row['equipment_name'] . '</h2>';
+        echo '<p>Type: ' . $row['equip_type'] . '</p>';
+        echo '<p>Quantity: ' . $row['quantity'] . '</p>';
+        echo '<p>Status: ' . ($row['equip_status'] ? 'Available' : 'Not Available') . '</p>';
+        echo '<img src="' . $row['equip_photo'] . '" alt="Equipment Photo" class="equipment-photo">';
+        // Add more details or actions as needed
+        echo '</div>';
+      }
+    } else {
+      echo 'No matching equipments found';
+    }
+  } else {
     $sql = "SELECT * FROM equipment";
     $result = $mysqli->query($sql);
 
     if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo '<div class="equipment">';
-            echo '<h2>' . $row['equipment_name'] . '</h2>'; 
-            echo '<p>Type: ' . $row['equip_type'] . '</p>';
-            echo '<p>Description: ' . $row['equip_description'] . '</p>';
-            echo '<p>Status: ' . ($row['equip_status'] ? 'Available' : 'Not Available') . '</p>';
-
-            // Display the image if available
-            if (!empty($row['equip_photo'])) {
-                echo '<img src="' . $row['equip_photo'] . '" alt="Equipment Photo">';
-            }
-
-            echo '<button>Edit Equipment</button>';
-            echo '</div>';
-        }
+      while ($row = $result->fetch_assoc()) {
+        echo '<div class="equipment">';
+        echo '<h2>' . $row['equipment_name'] . '</h2>';
+        echo '<p>Type: ' . $row['equip_type'] . '</p>';
+        echo '<p>Quantity: ' . $row['quantity'] . '</p>';
+        echo '<p>Status: ' . ($row['equip_status'] ? 'Available' : 'Not Available') . '</p>';
+        echo '<img src="' . $row['equip_photo'] . '" alt="Equipment Photo" class="equipment-photo">';
+        // Add more details or actions as needed
+        echo '</div>';
+      }
     } else {
-        echo 'No equipment available';
+      echo 'No equipments available';
     }
+  }
 
-    // Close the database connection
-    $mysqli->close();
-    ?>
-    </div>
+  if (isset($stmt)) {
+    $stmt->close();
+  }
 
-    <!-- Add Equipment button -->
-    <button class="add-equipment-btn" onclick="location.href='add_equipment.php'">Add Equipment</button>
+  $mysqli->close();
+  ?>
   </div>
 </body>
+<script src="../public/scripts/script.js"></script>
 
 </html>
-
--- custodian/scripts/room.php
+++ custodian/scripts/room.php
@@ -2,82 +2,169 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <title>Dashboard</title>
-  <link rel="stylesheet" href="../styles/room.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--Box Icons [https://boxicons.com/usage]-->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <title>Rooms</title>
+  <link rel="icon" href='../public/assets/images/r-icon.svg' type="image/svg">
+
+  <link rel="stylesheet" href="../public/styles/custodian_dashboard.css">
 </head>
 
 <body>
-  <div class="container">
-    <nav>
-      <ul>
-        <li><a href="#" class="logo">
-            <img src="/logo.jpg" alt="">
-            <span class="nav-item">DashBoard</span>
-          </a></li>
-        <li><a href="index.php">
-            <i class="fas fa-home"></i>
-            <span class="nav-item">Home</span>
-          </a></li>
-        <li><a href="room.php">
-            <i class="fas fa-user"></i>
-            <span class="nav-item">Add Rooms</span>
-          </a></li>
-        <li><a href="equipment.php">
-            <i class="fas fa-wallet"></i>
-            <span class="nav-item">Add Equipment</span>
-          </a></li>
-        <li><a href="manage_rental.php">
-            <i class="fas fa-chart-bar"></i>
-            <span class="nav-item">Manage Rental</span>
-          </a></li>
-          <li><a href="logout.php">
-            <i class="fas fa-chart-bar"></i>
-            <span class="nav-item">Logout</span>
-          </a></li>
+  <!--START OF SIDEBAR-->
+  <section id="sidebar">
+    <a href="#" alt="Rentify Logo" class="logo-img">
+      <img src='../public/assets/images/r-icon.svg' alt="Rentify Logo" class="logo-img">
+    </a>
+    <ul class="side-menu top">
+      <li>
+        <a href="../pages/custodian_dashboard.html">
+          <i class='bx bxs-dashboard'></i>
+          <span class="text">Dashboard</span>
+        </a>
+      </li>
+      <li class="active">
+        <a href="../scripts/room.php">
+          <i class='bx bxs-door-open'></i>
+          <span class="text">Rooms</span>
+        </a>
+      </li>
+      <li>
+        <a href="../scripts/equipment.php">
+          <i class='bx bxs-cabinet'></i>
+          <span class="text">Equipments</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-clipboard'></i>
+          <span class="text">Transaction History</span>
+        </a>
+      </li>
+      <ul class="side-menu bottom">
+        <li>
+          <a href="#" class="report">
+            <i class='bx bxs-shield-minus'></i>
+            <span class="text">Report</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class='bx bxs-cog'></i>
+            <span class="text">Settings</span>
+          </a>
+        </li>
+        <li>
+          <a href="../pages/custodian_login.html" class="logout">
+            <i class='bx bx-log-out'></i>
+            <span class="text">Logout</span>
+          </a>
+        </li>
       </ul>
+  </section>
+  <!--END OF SIDEBAR-->
+  <!--START OF CONTENT-->
+  <section id="content">
+    <nav>
+      <i class='bx bx-menu-alt-left'></i>
+      <a href="#" class="nav-link">Categories</a>
+      <form action="#">
+        <div class="form-input">
+          <form action="room.php" method="GET" class="search-form">
+            <div class="form-input">
+              <input type="search" name="search" placeholder="Search for Room or Equipment"
+                value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search']) : ''; ?>">
+              <button type="submit" class="search-btn"><i class='bx bx-search'></i></button>
+            </div>
+          </form>
+        </div>
+      </form>
+      <!--Notification Feature-->
+      <a href="#" class="notification">
+        <i class='bx bxs-bell'></i>
+        <!--UPDATEABLE BY NUMBER OF REQUESTS/ORDERS-->
+        <span class="num">8</span>
+      </a>
+      <!--Profile Feature-->
+      <a href="#" class="profile">
+        <i class='bx bxs-user-circle'></i>
+      </a>
     </nav>
-<?php
-        // Check for room addition success and display notification
-        if (isset($_SESSION['room_added']) && $_SESSION['room_added']) {
-            echo '<div class="notification">Room added successfully!</div>';
-            // Reset the session variable
-            unset($_SESSION['room_added']);
-        }
-?>
+  </section>
+  <!--END OF CONTENT-->
+  <!--ROOMS LIST-->
+  <?php
+  // Check for room addition success and display notification
+  if (isset($_SESSION['room_added']) && $_SESSION['room_added']) {
+    echo '<div class="notification">Room added successfully!</div>';
+    // Reset the session variable
+    unset($_SESSION['room_added']);
+  }
+  ?>
 
-    <div class="room-list">
+  <div class="room-list">
     <?php
-            include 'room_db.php';
+    include 'room_db.php';
 
-            // Retrieve and display room information
-            $sql = "SELECT * FROM rooms";
-            $result = $mysqli->query($sql);
+    // Check for search query
+    $search = isset($_GET['search']) ? '%' . $mysqli->real_escape_string($_GET['search']) . '%' : '';
 
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    echo '<div class="room">';
-                    echo '<h2>' . $row['room_no'] . '</h2>';
-                    echo '<p>' . $row['room_location'] . '</p>';
-                    echo '<p>TYPE: ' . $row['room_type'] . '</p>';
-                    echo '<p>CAPACITY: ' . $row['capacity'] . '</p>';
-                    echo '<p>' . ($row['room_status'] ? 'Available' : 'Not Available') . '</p>';
-                    echo '<img src="' . $row['room_photo'] . '" alt="Room Photo" class="room-photo">';
-                    echo '<button>Edit Room</button>';
-                    echo '</div>';
-                }
-            } else {
-                echo 'No rooms available';
-            }
+    // Retrieve and display room information with search filter if provided, otherwise show all rooms
+    if (!empty($search)) {
+      $sql = "SELECT * FROM rooms WHERE room_no LIKE ? OR room_location LIKE ? OR room_type LIKE ?";
+      $stmt = $mysqli->prepare($sql);
+      $stmt->bind_param('sss', $search, $search, $search);
+      $stmt->execute();
+      $result = $stmt->get_result();
 
-            $mysqli->close();
-            ?>
-    </div>
+      if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+          echo '<div class="room">';
+          echo '<h2>' . $row['room_no'] . '</h2>';
+          echo '<p>' . $row['room_location'] . '</p>';
+          echo '<p>TYPE: ' . $row['room_type'] . '</p>';
+          echo '<p>CAPACITY: ' . $row['capacity'] . '</p>';
+          echo '<p>' . ($row['room_status'] ? 'Available' : 'Not Available') . '</p>';
+          echo '<img src="' . $row['room_photo'] . '" alt="Room Photo" class="room-photo">';
+          echo '<a href="edit_room.php?id=' . $row['room_id'] . '" class="edit-room-btn">Edit Room</a>';
+          echo '</div>';
+        }
+      } else {
+        echo 'No matching rooms found';
+      }
+    } else {
+      $sql = "SELECT * FROM rooms";
+      $result = $mysqli->query($sql);
 
-    <!-- Add Room button -->
-    <button class="add-room-btn" onclick="location.href='add_room.php'">Add Room</button>
+      if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+          echo '<div class="room">';
+          echo '<h2>' . $row['room_no'] . '</h2>';
+          echo '<p>' . $row['room_location'] . '</p>';
+          echo '<p>TYPE: ' . $row['room_type'] . '</p>';
+          echo '<p>CAPACITY: ' . $row['capacity'] . '</p>';
+          echo '<p>' . ($row['room_status'] ? 'Available' : 'Not Available') . '</p>';
+          echo '<img src="' . $row['room_photo'] . '" alt="Room Photo" class="room-photo">';
+          echo '<a href="edit_room.php?id=' . $row['room_id'] . '" class="edit-room-btn">Edit Room</a>';
+          echo '</div>';
+        }
+      } else {
+        echo 'No rooms available';
+      }
+    }
+
+    if (isset($stmt)) {
+      $stmt->close();
+    }
+
+    $mysqli->close();
+    ?>
   </div>
+
+
 </body>
+<script src="../public/scripts/script.js"></script>
 
 </html>
